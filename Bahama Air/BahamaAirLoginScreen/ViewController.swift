bda402491c5e161e7dc10d2883b0b317c29189bd
Christopher Lee <hanernlee@gmail.com>
2018-02-19 17:30:52 +0800

    Chapter 9

--- Bahama Air/BahamaAirLoginScreen/ViewController.swift
+++ Bahama Air/BahamaAirLoginScreen/ViewController.swift
@@ -47,7 +47,7 @@ func roundCorners(layer: CALayer, toRadius: CGFloat) {
 class ViewController: UIViewController {
   
   // MARK: IB outlets
-  
+
   @IBOutlet var loginButton: UIButton!
   @IBOutlet var heading: UILabel!
   @IBOutlet var username: UITextField!
@@ -63,9 +63,11 @@ class ViewController: UIViewController {
   let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
   let status = UIImageView(image: UIImage(named: "banner"))
   let label = UILabel()
+  let info = UILabel()
   let messages = ["Connecting ...", "Authorizing ...", "Sending credentials ...", "Failed"]
   
   var statusPosition = CGPoint.zero
+    
   
   // MARK: view controller methods
   
@@ -92,6 +94,14 @@ class ViewController: UIViewController {
     status.addSubview(label)
     
     statusPosition = status.center
+    
+    info.frame = CGRect(x: 0.0, y: loginButton.center.y + 60.0, width: view.frame.size.width, height: 30)
+    info.backgroundColor = UIColor.clear
+    info.font = UIFont(name: "HelveticaNeue", size: 12.0)
+    info.textAlignment = .center
+    info.textColor = UIColor.white
+    info.text = "Tap on a field and enter username and password"
+    view.insertSubview(info, belowSubview: loginButton)
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -108,10 +118,12 @@ class ViewController: UIViewController {
     
     heading.layer.add(flyRight, forKey: nil)
     
+    flyRight.setValue(username.layer, forKey: "layer")
     flyRight.beginTime = CACurrentMediaTime() + 0.3
     username.layer.add(flyRight, forKey: nil)
     username.layer.position.x = view.bounds.size.width/2
     
+    flyRight.setValue(password.layer, forKey: "layer")
     flyRight.beginTime = CACurrentMediaTime() + 0.4
     password.layer.add(flyRight, forKey: nil)
     password.layer.position.x = view.bounds.size.width/2
@@ -150,6 +162,12 @@ class ViewController: UIViewController {
         self.loginButton.center.y += 30.0
         self.loginButton.alpha = 1.0
     }, completion: nil)
+    
+    let flyLeft = CABasicAnimation(keyPath: "position.x")
+    flyLeft.fromValue = info.layer.position.x + view.frame.size.width
+    flyLeft.toValue = info.layer.position.x
+    flyLeft.duration = 5.0
+    info.layer.add(flyLeft, forKey: "infoappear")
   }
   
   // MARK: further methods
@@ -246,6 +264,19 @@ class ViewController: UIViewController {
 
 extension ViewController: CAAnimationDelegate {
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        print("Animation did finish")
+        guard let name = anim.value(forKey: "name") as? String else {
+            return
+        }
+        
+        if name == "form" {
+            let layer = anim.value(forKey: "layer") as? CALayer
+            anim.setValue(nil, forKey: "layer")
+            
+            let pulse = CABasicAnimation(keyPath: "transform.scale")
+            pulse.fromValue = 1.25
+            pulse.toValue = 1.0
+            pulse.duration = 0.25
+            layer?.add(pulse, forKey: nil)
+        }
     }
 }
