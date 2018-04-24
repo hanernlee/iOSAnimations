ad2df64f5e60f348dcb503d0a52df5986e972b92
Christopher Lee <hanernlee@gmail.com>
2018-04-24 22:55:25 +1000

    Chapter 23

---
+++ Chapter 23/starter/LockSearch/PresentTransition.swift
@@ -0,0 +1,19 @@
+//
+//  PresentTransition.swift
+//  Widgets
+//
+//  Created by Christopher Lee on 24/4/18.
+//  Copyright © 2018 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+
+class PresentTransition: NSObject, UIViewControllerAnimatedTransitioning {
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0.75
+  }
+  
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    <#code#>
+  }
+}
