653e2a8842b862a5c0cc652eb1c0ad4973f668d4
Christopher Lee <hanernlee@gmail.com>
2018-04-25 11:11:30 +1000

    WIP

--- Chapter 23/starter/LockSearch/PresentTransition.swift
+++ Chapter 23/starter/LockSearch/PresentTransition.swift
@@ -14,7 +14,7 @@ class PresentTransition: NSObject, UIViewControllerAnimatedTransitioning {
   }
   
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-    <#code#>
+    transitionAnimator(using: transitionContext).startAnimation()
   }
   
   func transitionAnimator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
@@ -25,5 +25,20 @@ class PresentTransition: NSObject, UIViewControllerAnimatedTransitioning {
     container.addSubview(to)
     to.transform = CGAffineTransform(scaleX: 1.33, y: 1.33).concatenating(CGAffineTransform(translationX: 0.0, y: 200))
     to.alpha = 0
+    
+    let animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut)
+    animator.addAnimations ({
+      to.transform = CGAffineTransform(translationX: 0.0, y: 100)
+    }, delayFactor: 0.15)
+    
+    animator.addAnimations({
+      to.alpha = 1.0
+    }, delayFactor: 0.5)
+    
+    animator.addCompletion { _ in
+      transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+    }
+    
+    return animator
   }
 }
